ca422967c9b232c0b5b95385d83b67eb493c2367
ljw2455 <97268289+ljw2455qq@users.noreply.github.com>
2025-08-14 22:15:28 +0900

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,12 @@
 // Firebase 프로젝트 설정을 입력하세요.
 const FIREBASE_CONFIG = {
-  apiKey: "YOUR_FIREBASE_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT.firebaseio.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID"
+    apiKey: "AIzaSyA5hMmN9QFgdFtaA5gicS_pj-blu_jJdvE",
+    authDomain: "rider-bf48b.firebaseapp.com",
+    databaseURL: "https://rider-bf48b-default-rtdb.firebaseio.com",
+    projectId: "rider-bf48b",
+    storageBucket: "rider-bf48b.firebasestorage.app",
+    messagingSenderId: "1026929653322",
+    appId: "1:1026929653322:web:b90541cccba5b4186198b3"
 };
 
 // URL 파라미터로 driverId, zoom, style, lang, owmKey 등을 받을 수 있습니다.
